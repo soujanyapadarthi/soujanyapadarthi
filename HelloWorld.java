6cfed3a33d53bc0947e47854fa8d508c80d0336d
soujanyapadarthi <soujanya.padarthi@gmail.com>
2024-08-31 22:45:19 +0530

    commit all files

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        
+        System.out.println("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon 
+            System.out.println("i = " + i);
+        }
+    }
+}
