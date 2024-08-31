51dac1d47e4a1aa61553f2b388911f87d7895c8e
soujanyapadarthi <soujanya.padarthi@gmail.com>
2024-08-31 22:51:02 +0530

    another commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         
-        System.out.println("Hello and welcome!");
+        System.out.println(10/3);
 
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon 
